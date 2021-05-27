7a499e9edf696fd4f435499cde97d04d74906f51
bastien.andres <b.andres@net.estia.fr>
2021-05-27 15:27:05 +0200

    BDD connecté et fonction Create OK

---
+++ CalculEncours.vb
@@ -0,0 +1,6 @@
+﻿Public Class CalculEncours
+    Dim libelle As String
+    Dim nbPlaque As Double
+
+
+End Class
--- Connexion.vb
+++ Connexion.vb
@@ -4,21 +4,10 @@ Imports System.Data.OleDb
 Public Class Connexion
     Private connString As String
     Private oleConnection As OleDbConnection
-    Private oleCommande As OleDbCommand
-    Private oleAdapteur As OleDbDataAdapter
-    Private dataSet As DataSet
-
     Public Function OuvertureBDD() As OleDbConnection
-        connString = "Provider=Microsoft.ACE.OLDB.12.0;"
-        'connString += "Jet OLEDB:Database Password = password"
-        'connString += "Data Source=C:\Users\bastien.andres\Documents\BDDTestIndic.mdb"
+        connString = "Provider=Microsoft.ACE.OLEDB.12.0;"
         connString += "Data Source=C:\Users\bastien.andres\Documents\IndicateurPressageBDD.accdb"
         oleConnection = New OleDbConnection(connString)
-        'oleCommande = New OleDbCommand(requete, oleConnection)
-        'oleAdapteur = New OleDbDataAdapter(oleCommande)
-        'dataSet = New DataSet()
-        'oleAdapteur.Fill(dataSet, "Resultat")
         Return oleConnection
     End Function
-
 End Class
